730103e64eddc32973a987deb4a4816b0759f462
Getsemani Villegas <gvillegasg1601@alumno.ipn.mx>
2025-05-23 01:04:02 -0600

    Create setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='greedy-fire-sim',
+    version='0.1.0',
+    description='A greedy simulation for firefighter problem',
+    author='Getsemani Villegas',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'numpy',
+        'networkx',
+        'matplotlib',
+        'pandas',
+        'scipy',
+        'tqdm',
+        'jsonschema'
+    ],
+    python_requires='>=3.6',
+)
