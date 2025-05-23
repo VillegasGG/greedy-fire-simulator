a53608d6753e1461a2b8346a00122112ab9645db
Getsemani Villegas <gvillegasg1601@alumno.ipn.mx>
2025-05-23 02:01:12 -0600

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='greedyff',
-    version='0.1.0',
+    version='0.2.0',
     description='A greedy simulation for firefighter problem',
     author='Getsemani Villegas',
     packages=find_packages(where='src'),
